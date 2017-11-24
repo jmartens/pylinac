44886a2021ea6a7d728122e6863b0e02d171d403
Jonathan Martens <jonathan.martens@maastro.nl>
2017-11-24 20:26:35 +0100

    Fix typo in documentation

--- pylinac/ncs18.py
+++ pylinac/ncs18.py
@@ -4,7 +4,7 @@ NCS-18 absolute linac dose calibration.
 Functions include all relevant calculations for NCS-18. Where values/equations are used they are specified in the
 documentation.
 
-Classes include photon, using cylindical and electron calibrations using plan-parallel chambers. Pass all the relevant
+Classes include photon, using cylindrical and electron calibrations using plan-parallel chambers. Pass all the relevant
 raw measurements and the class will compute all corrections and corrected readings and dose at 10cm and dmax/dref.
 """
 
